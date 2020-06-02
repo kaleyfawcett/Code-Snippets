f4d29240e66f3aacf5339568f49787662935aeb0
kaleyfawcett <kaley.fawcett@gmail.com>
2020-06-02 19:52:45 -0400

    made some models and such

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import User 
 
-# Create your models here.
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, unique=True) 
+
+    def __str__(self):
+        return self.tag 
+
+class CodeSnippet(models.Model): 
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name= 'codesnippets')
+    title = models.CharField(max_length=250, blank=True, default= '')
+    language = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    code_body = models.TextField()
+    is_public = models.BooleanField(default=True)
+    parent = models.ForeignKey(to ='self', on_delete=models.SET_NULL, related_name='children', null=True, blank =True)
+    tags = models.ManyToManyField(to=Tag, related_name='codesnippets')
+
+    def __str__(self):
+        return self.title 
